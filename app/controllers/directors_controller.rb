0c79e987cfe33dc66bf6d7fcb3b3e9d628cda4bd
manthon0 <manthon0@chicagobooth.edu>
2022-11-04 23:26:43 +0000

    eldest and director pages

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,7 +6,16 @@ render({:template => "director_templates/index.html.erb"})
 end
 
 def wisest
+  @list_of_directors = Director.all
+  @oldest_director = @list_of_directors.where.not({:dob => nil}).order(:dob).first.name
+  @oldest_director_dob = @list_of_directors.order(:dob).where.not({:dob => nil}).first.dob
 render({:template => "director_templates/eldest.html.erb"})
 end
   
+def director_details
+current_id = params.fetch("an_id")
+@the_director = Director.where({:id => current_id}).first
+render({:template => "director_templates/show.html.erb"})
+end
+
 end
